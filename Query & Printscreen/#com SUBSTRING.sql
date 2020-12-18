05a5be5f64d04bffc4acb060eaf19688bdb77861
git-atoms <atom.sliwinski@gmail.com>
2020-12-18 21:59:41 +0100

    Update #com SUBSTRING.sql
    Made it more clear for eyes

--- Query & Printscreen/#com SUBSTRING.sql
+++ Query & Printscreen/#com SUBSTRING.sql
@@ -2,16 +2,5 @@ SELECT
 	substring(LastName,2,5)
 
 
-
---		+' ma na imię '+
---	FirstName
---		as 'Imię i Nazwisko'
 FROM
 	[AdventureWorks2014].Person.Person
-	
---WHERE
---	FirstName > 'aa'
-
---ORDER BY
---	FirstName
-	
