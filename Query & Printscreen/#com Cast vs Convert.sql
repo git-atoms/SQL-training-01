f151c56aa9f3b313797cd83edf9c528fc3cf7879
git-atoms <atom.sliwinski@gmail.com>
2020-12-19 08:58:34 +0100

    Create #com Cast vs Convert
    SQL query with printscreen example

---
+++ Query & Printscreen/#com Cast vs Convert.sql
@@ -0,0 +1,6 @@
+select StoreID
+--, cast(StoreID as float)/3 as 'Wartoœæ zmiennoprzecinkowa'
+--, convert(float,StoreID)/3 as 'Wartoœæ zmiennoprzecinkowa'
+
+from
+[AdventureWorks2014].Sales.Customer
