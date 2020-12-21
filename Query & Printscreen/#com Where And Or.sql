f68f7ce9dc9d703af542a8c3106c876db3c3549a
git-atoms <atom.sliwinski@gmail.com>
2020-12-21 05:53:54 +0100

    Update #com Where And Or.sql

--- Query & Printscreen/#com Where And Or.sql
+++ Query & Printscreen/#com Where And Or.sql
@@ -1,7 +1,6 @@
 use [AdventureWorks2014];
 --
 
-
 SELECT
 JobTitle
 ,
@@ -16,9 +15,10 @@ FROM
 HumanResources.Employee
 
 WHERE
-MaritalStatus = 'M'
+Gender = 'M'
 
-AND HireDate > '20071111'
-OR BirthDate < '19620913'
+-- data w formacie RRRR-MM-DD
+AND HireDate > '2007' --data zatrudnienia wiêksza ni¿ rok 2007
+OR BirthDate < '1962' --data urodzenia mniejsza ni¿ rok 1962
 
 --AND (HireDate > '20071111' OR BirthDate < '19620913')
