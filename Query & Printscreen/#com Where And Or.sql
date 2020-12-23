7aa251e12a1b7e7a571d8ce65b1b16f8f40d3bf5
git-atoms <atom.sliwinski@gmail.com>
2020-12-23 23:37:56 +0100

    Update #com Where And Or.sql

--- Query & Printscreen/#com Where And Or.sql
+++ Query & Printscreen/#com Where And Or.sql
@@ -1,3 +1,8 @@
+/*
+Polecenie USE w odniesieniu do wybrania bazy danych dziaÅ‚a tylko
+w localhost.
+*/
+
 use [AdventureWorks2014];
 --
 
@@ -18,7 +23,7 @@ WHERE
 Gender = 'M'
 
 -- data w formacie RRRR-MM-DD
-AND HireDate > '2007' --data zatrudnienia wiêksza ni¿ rok 2007
-OR BirthDate < '1962' --data urodzenia mniejsza ni¿ rok 1962
+AND HireDate > '2007' --data zatrudnienia wiï¿½ksza niï¿½ rok 2007
+OR BirthDate < '1962' --data urodzenia mniejsza niï¿½ rok 1962
 
 --AND (HireDate > '20071111' OR BirthDate < '19620913')
