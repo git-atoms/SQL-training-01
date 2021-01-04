64892f02a7131bc7c338de1fee81c99bd0aa60ac
git-atoms <atom.sliwinski@gmail.com>
2021-01-04 05:52:19 +0100

    Create #com Cast + Round + Join
    Screen with SQL query

---
+++ Query & Printscreen/#com Cast + Round + Join.sql
@@ -0,0 +1,26 @@
+SELECT
+
+s.FirstName +' '+ s.LastName AS 'Imię i nazwisko'
+,
+e.BirthDate AS 'Data urodzenia'
+,
+e.HireDate AS 'Zatrudniony od'
+,
+--CAST(e.VacationHours AS FLOAT)/8
+ROUND((CAST(e.VacationHours AS FLOAT)/8),2)
+
+AS 'Pozostało dni urlopu'
+
+
+FROM
+Person.Person AS s
+
+JOIN
+HumanResources.Employee as e
+ON
+e.BusinessEntityID=s.BusinessEntityID
+
+ORDER BY
+[Pozostało dni urlopu] desc
+,
+[Imię i nazwisko]
