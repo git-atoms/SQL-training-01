e67be7266080da3c185d29772efb3445a0f9301a
git-atoms <atom.sliwinski@gmail.com>
2021-01-30 07:59:48 +0100

    Update #com Where And Or.sql

--- Query & Printscreen/#com Where And Or.sql
+++ Query & Printscreen/#com Where And Or.sql
@@ -23,7 +23,7 @@ WHERE
 Gender = 'M'
 
 -- data w formacie RRRR-MM-DD
-AND HireDate > '2007' --data zatrudnienia wi�ksza ni� rok 2007
-OR BirthDate < '1962' --data urodzenia mniejsza ni� rok 1962
+AND HireDate > '2007' --data zatrudnienia większa niż rok 2007
+OR BirthDate < '1962' --data urodzenia mniejsza niż rok 1962
 
 --AND (HireDate > '20071111' OR BirthDate < '19620913')
