fb4d651675be7ee417eea66501c7c62b701b3bde
git-atoms <atom.sliwinski@gmail.com>
2021-07-07 17:30:10 +0200

    Update conversion.sql

--- conversion.sql
+++ conversion.sql
@@ -1,3 +1,9 @@
-select
-GETDATE() as "Data i godzina",
-cast(getdate() as date) as "Tylko data"
+--select
+--GETDATE() as "Data i godzina",
+--cast(getdate() as date) as "Tylko data"
+--;
+
+--select cast(NationalIDNumber as decimal) as Numer
+--from HumanResources.Employee
+--order by 1 asc
+--;
