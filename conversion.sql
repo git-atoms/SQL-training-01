61ab9d6697d16ac38e465de0086b69a0c07f1c28
git-atoms <atom.sliwinski@gmail.com>
2021-07-07 18:16:20 +0200

    Update conversion.sql

--- conversion.sql
+++ conversion.sql
@@ -1,9 +1,13 @@
 --select
 --GETDATE() as "Data i godzina",
---cast(getdate() as date) as "Tylko data"
+--cast(getdate() as date) as "Tylko data bez godziny"
 --;
 
 --select cast(NationalIDNumber as decimal) as Numer
 --from HumanResources.Employee
 --order by 1 asc
 --;
+
+select BirthDate as "Data urodzin",
+datename(MONTH, BirthDate) + ' ' +datename(weekday, BirthDate) as "Jaki to by³ miesi¹c i dzieñ tygodnia?"
+from HumanResources.Employee
