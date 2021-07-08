96141ec323778c0fa3c5b9cd1b7927de755af23a
git-atoms <atom.sliwinski@gmail.com>
2021-07-08 17:33:11 +0200

    Update validation.sql

--- validation.sql
+++ validation.sql
@@ -1,14 +1,18 @@
 --select top(5) *
 --from HumanResources.Employee
-
+--;
 		
 --select distinct ISnull(cast(hiredate as char), 'noNulls') as "Kolumna z nullem"
 --from HumanResources.Employee
+--;
 
-select distinct
-nullif(MaritalStatus, Gender) as 'Column'
-from HumanResources.Employee
 
-where
-'Column' is not null
-order by 1 desc
+--select distinct
+--nullif(MaritalStatus, Gender) as 'Column'
+--from HumanResources.Employee
+--where
+--'Column' is not null
+--order by 1 desc
+--;
+
+
