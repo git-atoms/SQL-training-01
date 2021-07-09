1b8ff0ef4ba38fd6c9d04639415d6d5d10b43c61
git-atoms <atom.sliwinski@gmail.com>
2021-07-09 15:39:30 +0200

    Update rounds.sql

--- rounds.sql
+++ rounds.sql
@@ -1,5 +1,5 @@
---select round(2.125212, 6, 1)
+--select round(2.125212, 6, 1) --wartoœæ mia³aby byæ zaokr¹glona do 6go miejsca po przecinku ale warunek 1 w nawiasie obcina na sztywno i nie zaokr¹gla
 
---select FLOOR(2.125212)
+--select FLOOR(2.125212) --wynik 2
 
-select ceiling(2.125212)
+select ceiling(2.125212) --wynik 3
