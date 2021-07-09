a4317c1a36f954882931fa08c44eb51be089f4dc
git-atoms <atom.sliwinski@gmail.com>
2021-07-09 04:33:00 +0200

    Update arithmetic.sql

--- arithmetic.sql
+++ arithmetic.sql
@@ -1,4 +1,9 @@
-select Rate,
-Rate + 10 as "Wiêcej o 10",
-Rate / 2 as "Zmniejszone o po³owê"
-from HumanResources.EmployeePayHistory
+--select Rate,
+--Rate + 10 as "Wiêcej o 10",
+--Rate / 2 as "Zmniejszone o po³owê"
+--from HumanResources.EmployeePayHistory
+
+
+select cast(3500 as float) / 1500 as "Dzielenie floatów"
+
+select 3500 % 1500 as "Modulo z dzielenia" -- 2x1500=3000 (modulo zostaje 500)
