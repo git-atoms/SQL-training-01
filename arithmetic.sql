e62fbd77395ad573bc862e8050975556e2cd884b
git-atoms <atom.sliwinski@gmail.com>
2021-07-09 04:34:42 +0200

    Update arithmetic.sql

--- arithmetic.sql
+++ arithmetic.sql
@@ -4,6 +4,11 @@
 --from HumanResources.EmployeePayHistory
 
 
+/* Reszta z dzielenia i modulo
+
 select cast(3500 as float) / 1500 as "Dzielenie floatów"
 
-select 3500 % 1500 as "Modulo z dzielenia" -- 2x1500=3000 (modulo zostaje 500)
+select 3500 % 1500 as "Modulo z dzielenia" -- 2x1500=3000 (modulo zostaje 500 bo 3500-3000=500)
+
+*/
+
