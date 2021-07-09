8a512b289b15d6b53103749de76d86d29b0f9c7a
git-atoms <atom.sliwinski@gmail.com>
2021-07-09 17:15:42 +0200

    Update rounds.sql

--- rounds.sql
+++ rounds.sql
@@ -1,7 +1,9 @@
 select
 2.12567 as "Wartoœæ wyjœciowa",
-ROUND(2.12567, 0) as "Bez zaokr¹glenia",
-ROUND(2.12567, 3) as "Zaokr¹glenie do trzech miejsc",
-ROUND(2.12567, 3, 1) as "Odciêcie po trzeciej jak jest",
-FLOOR(2.12567) as "Obni¿enie do pe³nej",
-CEILING(2.12567) as "Podniesienie do pe³nej"
+ROUND(2.15567, 0) as "Zaokr¹glenie do pe³nej",
+ROUND(2.15567, 1) as "Zaokr¹glenie do jednego miejsca",
+ROUND(2.15567, 3) as "Zaokr¹glenie do trzech miejsc",
+ROUND(2.15567, 3, 1) as "Odciêcie po trzeciej jak jest",
+ROUND(215.15567, -1) as "Zaokr¹glenie przed przecinkiem",
+FLOOR(2.15567) as "Obni¿enie do pe³nej",
+CEILING(2.15567) as "Podniesienie do pe³nej"
