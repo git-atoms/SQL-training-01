bc177fdb826645c979426bda6be3fec8ef546c50
git-atoms <atom.sliwinski@gmail.com>
2021-07-12 16:52:03 +0200

    Update grupowanie.sql

--- grupowanie.sql
+++ grupowanie.sql
@@ -1,12 +1,14 @@
 --select
 --sum(VacationHours) as "Suma wszystkich godzin urlopu"
 --from HumanResources.Employee
-
+--group by JobTitle --(chocia¿ nie wystêpuje w SELECT to i tak mo¿na pogrupowaæ po nazwie stanowiska)
 
 select
 JobTitle,
 sum(VacationHours) as "Suma godzin urlopu dla stanowiska"
 from HumanResources.Employee
 group by JobTitle
-having sum(VacationHours) > 17
-order by [Suma godzin urlopu dla stanowiska] asc
+--having sum(VacationHours) > 17
+having (JobTitle) > 'c'  --(wiêksze od oznacza tu wiêksze-równe)
+--order by [Suma godzin urlopu dla stanowiska] asc
+order by JobTitle asc
