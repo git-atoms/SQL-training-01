844ada11f43c623d913332f71c240d2c12ed6dd1
git-atoms <atom.sliwinski@gmail.com>
2021-07-12 17:59:43 +0200

    Update kolumnaWarunkowa.sql

--- kolumnaWarunkowa.sql
+++ kolumnaWarunkowa.sql
@@ -1,9 +1,12 @@
 select distinct Name,
 
 	case name
-	when 'French' then 'Francuz'
+	when 'french' then 'Francuz' --nie jest CS
 	when 'Hebrew' then '¯yd'
 	when 'Spanish' then 'Hiszpan'
+	when 'Arabic' then cast(getdate() as char) --muszê mieæ "then" musi mieæ ten sam typ co "when"
+	else ''
 	end as "Obcokrajowcy"
 
 from Production.Culture
+order by Obcokrajowcy asc
