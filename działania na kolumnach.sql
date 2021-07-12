f4dd9268639dc368570c9be3d9c4eb78fc291e6c
git-atoms <atom.sliwinski@gmail.com>
2021-07-12 04:39:26 +0200

    Update działania na kolumnach.sql

---
+++ działania na kolumnach.sql
@@ -0,0 +1,12 @@
+select
+FirstName + ' ' + LastName as "Imiê i nazwisko",
+VacationHours/8 as "Pozosta³o dni urlopu",
+SickLeaveHours/8 "Pozosta³o dni na L4"
+from HumanResources.Employee as s
+
+join
+Person.Person as p
+on
+p.BusinessEntityID=s.BusinessEntityID
+
+order by 1 asc
