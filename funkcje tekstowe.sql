24eb0b62b5e1a34f82a004db7514faf89f6f2c50
git-atoms <atom.sliwinski@gmail.com>
2021-07-14 04:43:21 +0200

    Create funkcje tekstowe.sql

---
+++ funkcje tekstowe.sql
@@ -0,0 +1,7 @@
+select LOWER('Uczê siê SQL-a') as "Ma³e litery"
+
+select UPPER('Uczê siê SQL-a') as "Kapitaliki"
+
+select LEN('Uczê siê SQL-a') as "D³ugoœæ tekstu"
+
+select LEFT('Uczê siê SQL-a',)
