a94f781237eb5c0639df2a6a4b232d8b28c62fbd
git-atoms <atom.sliwinski@gmail.com>
2021-07-14 04:45:02 +0200

    Update funkcje tekstowe.sql

--- funkcje tekstowe.sql
+++ funkcje tekstowe.sql
@@ -4,4 +4,6 @@ select UPPER('Uczê siê SQL-a') as "Kapitaliki"
 
 select LEN('Uczê siê SQL-a') as "D³ugoœæ tekstu"
 
-select LEFT('Uczê siê SQL-a',)
+select LEFT('Uczê siê SQL-a', 7) as "Wyœwietla z lewej"
+
+select RIGHT('Uczê siê SQL-a', 8) as "Wyœwietla z prawej"
