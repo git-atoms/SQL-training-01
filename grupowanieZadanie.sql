3c370dd2f6685c677d8c2eeb61514bab491f22be
git-atoms <atom.sliwinski@gmail.com>
2021-07-16 05:38:02 +0200

    Update grupowanieZadanie.sql

--- grupowanieZadanie.sql
+++ grupowanieZadanie.sql
@@ -5,3 +5,9 @@ count(*) as "Liczba takich samych nazwisk"
 from Person.Person
 group by LastName
 order by 2 desc
+;
+
+select
+count(LineTotal)
+from Sales.SalesOrderDetail
+where LineTotal between '5000' and '6000'
