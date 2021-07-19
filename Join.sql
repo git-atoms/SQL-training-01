caa52dcea0602e81a03079bfce0a80f974dd6a74
git-atoms <atom.sliwinski@gmail.com>
2021-07-19 05:03:38 +0200

    Update Join.sql

--- Join.sql
+++ Join.sql
@@ -1,4 +1,7 @@
--- CROSS JOIN (nawet nie trzeba u¿ywac klauzuli JOIN, wystarczy podaæ dwa Ÿród³a przy FROM)
+/*CROSS JOIN (nawet nie trzeba u¿ywac klauzuli JOIN, wystarczy podaæ dwa Ÿród³a przy FROM).
+Wtedy wartoœci w z³¹czonej tabeli siê zwiêkszaj¹ bo nastêpuje mno¿enie iloœci danych z pierwszej tabeli/widoku
+z iloœci¹ danych z drugiej tabeli/widoku.
+*/
 
 select *
 from
