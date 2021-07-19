1c9ab05d627ab0b61f13f76adfdf3fe790ee4934
git-atoms <atom.sliwinski@gmail.com>
2021-07-19 05:06:31 +0200

    Update Join.sql

--- Join.sql
+++ Join.sql
@@ -1,9 +1,10 @@
 /*CROSS JOIN (nawet nie trzeba u¿ywac klauzuli JOIN, wystarczy podaæ dwa Ÿród³a przy FROM).
 Wtedy wartoœci w z³¹czonej tabeli siê zwiêkszaj¹ bo nastêpuje mno¿enie iloœci danych z pierwszej tabeli/widoku
-z iloœci¹ danych z drugiej tabeli/widoku.
+z iloœci¹ danych z drugiej tabeli/widoku. Dlatego trzeba u¿yæ filtra WHERE.
 */
 
 select *
 from
-HumanResources.EmployeeDepartmentHistory,
-HumanResources.Shift
+HumanResources.EmployeeDepartmentHistory as dh,
+HumanResources.Shift as s
+where dh.ShiftID=s.ShiftID
