f46e8ad4ce59f9493544684f171eeaaa2623076b
git-atoms <atom.sliwinski@gmail.com>
2021-07-19 05:26:22 +0200

    Update j_LeftJoin.sql

--- j_LeftJoin.sql
+++ j_LeftJoin.sql
@@ -1,1 +1,11 @@
---LEFT JOIN (LEFT OUTER JOIN)
+/*LEFT JOIN (LEFT OUTER JOIN)
+Zwraca wszystkie rekordy z tabeli pierwszej i tylko te pasuj¹ce z tabeli drugiej.
+*/
+
+select *
+from
+Person.Person as pp
+
+left join
+Person.BusinessEntity as pbe
+on pbe.BusinessEntityID=pp.BusinessEntityID
