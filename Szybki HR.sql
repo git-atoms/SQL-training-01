21121bbb0396ff6b44e53d1d0774b9d2282bdc27
git-atoms <atom.sliwinski@gmail.com>
2021-08-09 06:22:21 +0200

    Create Szybki HR.sql

---
+++ Szybki HR.sql
@@ -0,0 +1,31 @@
+select
+pp.FirstName +' '+pp.LastName as "Imiê i nazwisko",
+hre.BirthDate as "Data urodzin",
+hre.NationalIDNumber,
+hre.VacationHours as "Pozosta³o godzin urlopu",
+hre.VacationHours/8 as "Pozosta³o dni urlopu",
+hre.SickLeaveHours as "Przechorowanych godzin",
+hre.SickLeaveHours/8 as "Przechorowanych dni",
+hrve.JobTitle,
+hrve.PhoneNumber,
+hrve.AddressLine1,
+hrvedh.StartDate as "Data zatrudnienia",
+DATEDIFF(year, hrvedh.StartDate,GETDATE()) as "Lat pracy"
+
+--into HumanResources.SzybkiHR
+
+from Person.Person as pp
+inner join
+HumanResources.Employee as hre
+on
+hre.BusinessEntityID = pp.BusinessEntityID
+inner join
+HumanResources.vEmployee as hrve
+on
+hrve.BusinessEntityID=pp.BusinessEntityID
+inner join
+HumanResources.vEmployeeDepartmentHistory as hrvedh
+on
+hrvedh.BusinessEntityID=pp.BusinessEntityID
+
+order by pp.FirstName
