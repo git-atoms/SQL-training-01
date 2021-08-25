8dca79dd848b5972caef0f9f1966210d304f18c5
git-atoms <atom.sliwinski@gmail.com>
2021-08-25 08:32:41 +0200

    Create With.sql

---
+++ With.sql
@@ -0,0 +1,22 @@
+with Sale as
+(select
+FirstName +' '+LastName as "Imiê i nazwisko",
+SalesYTD
+from Sales.SalesPerson as s
+
+inner join
+HumanResources.Employee as e
+on
+e.BusinessEntityID=s.BusinessEntityID
+
+inner join
+HumanResources.vEmployee as ve
+on
+ve.BusinessEntityID=s.BusinessEntityID)
+
+select top 3
+[Imiê i nazwisko],
+max(SalesYTD) as "Najwy¿sza sprzeda¿"
+from Sale
+group by [Imiê i nazwisko]
+order by 2 desc
