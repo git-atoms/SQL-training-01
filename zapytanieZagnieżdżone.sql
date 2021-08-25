c60d1d0e7498cb82e9dcea8db7f3df25ee5d6494
git-atoms <atom.sliwinski@gmail.com>
2021-08-25 08:32:43 +0200

    Update zapytanieZagnieżdżone.sql

--- zapytanieZagnieżdżone.sql
+++ zapytanieZagnieżdżone.sql
@@ -1,20 +1,25 @@
-select
+select top 3
+sale.[Imiê i nazwisko],
 max(salesytd) as "Najwy¿sza sprzeda¿",
 min(salesytd) as "Najni¿sza sprzeda¿",
 AVG(salesytd) as "Œrednia sprzeda¿"
-from
+
+from 
 
 (select
 FirstName +' '+LastName as "Imiê i nazwisko",
 SalesYTD
 from Sales.SalesPerson as s
 
-join
+inner join
 HumanResources.Employee as e
 on
 e.BusinessEntityID=s.BusinessEntityID
 
-join
+inner join
 HumanResources.vEmployee as ve
 on
 ve.BusinessEntityID=s.BusinessEntityID) as Sale
+
+group by sale.[Imiê i nazwisko]
+order by 2 desc
